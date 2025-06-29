81843b5feafd9634dbd811949681f455080411a2
pangiiz-qq <phebe.phiphi@gmail.com>
2025-06-29 14:23:05 +0700

    edit all js file

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James"
+const totalPrice = 15000;
+const branchTotal = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+branchTotal = 2500;
+
+console.log(firstName+" "+lastName);
+console.log(address);
+console.log(restaurantName+" ("+branchTotal+" branchs)");
+console.log("Total "+totalPrice)
